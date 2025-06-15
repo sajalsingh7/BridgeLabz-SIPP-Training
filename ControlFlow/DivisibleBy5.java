6bdf2440630da528aba43757a9a81081fe7903dd
Sajal <sajalsingh2704@gmail.com>
2025-06-15 19:21:19 +0530

    [Sajal] divisible by 5

--- ControlFlow/DivisibleBy5.java
+++ ControlFlow/DivisibleBy5.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class DivisibleBy5{
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
-System.out.print("Enter a number: ");
+System.out.print("Enter  num: ");
 int num=sc.nextInt();
 if(num%5==0){
 System.out.println("Is the number "+num+" divisible by 5? Yes");
