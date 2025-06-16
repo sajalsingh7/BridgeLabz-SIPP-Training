f8985e54c17e33a3fac50f78b96a42f0599bce10
Sajal <sajalsingh2704@gmail.com>
2025-06-16 10:00:43 +0530

    [Sajal] spring season

---
+++ ControlFlow/SpringSeason.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class SpringSeason {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter month : ");
+int month = sc.nextInt();
+System.out.print("Enter day : ");
+int day = sc.nextInt();
+
+boolean spring = (month == 3 && day >= 20) || (month == 4) || (month == 5) || (month == 6 && day <= 20);
+if(spring) {
+System.out.println("Spring Season");
+} else {
+System.out.println("Not a Spring Season");
+}
+}
+}
