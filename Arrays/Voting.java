75ae2f39d6c84749333b385f85420673ab8c5185
Sajal <sajalsingh2704@gmail.com>
2025-06-17 00:34:33 +0530

    [Sajal] student can vote or not

---
+++ Arrays/Voting.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Voting {
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int[] ages=new int[10];
+
+for(int i=0;i<10;i++) {
+System.out.print("Enter age of student "+(i+1)+": ");
+ages[i]=sc.nextInt();
+}
+
+for(int age:ages) {
+if(age<0) {
+System.out.println("Invalid age: "+age);
+} else if(age>=18) {
+System.out.println("The student with the age "+age+" can vote");
+} else {
+System.out.println("The student with the age "+age+" cannot vote");
+}
+}
+}
+}
