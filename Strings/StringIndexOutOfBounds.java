d8a70e932febceaf66c9e68f0dba08d64ab2da7f
Sajal <sajalsingh2704@gmail.com>
2025-06-18 12:18:14 +0530

    [Sajal] demonstrate String Index Out Of Bounds Exception

---
+++ Strings/StringIndexOutOfBounds.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class StringIndexOutOfBounds {
+    
+    public static void generateException(String text) {
+        System.out.println(text.charAt(text.length()));
+    }
+    
+    public static void handleException(String text) {
+        try {
+            System.out.println(text.charAt(text.length()));
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Caught StringIndexOutOfBoundsException: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("Caught RuntimeException: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.next();
+
+        System.out.println("Demonstrating StringIndexOutOfBoundsException handling:");
+        handleException(input);
+    }
+}
