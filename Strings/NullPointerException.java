81d93b00757b10917cc9a88a4497a79122501011
Sajal <sajalsingh2704@gmail.com>
2025-06-18 09:57:07 +0530

    [Sajal] strings folder

---
+++ Strings/NullPointerException.java
@@ -0,0 +1,22 @@
+public class NullPointerException {
+
+  
+    public static void generateNullPointerException() {
+        String text = null; 
+        System.out.println("Length of text: " + text.length());
+    }
+
+    public static void handleNullPointerException() {
+        String text = null;
+        try {
+            System.out.println("Length of text: " + text.length());
+        } catch (NullPointerException e) {
+            System.out.println("Caught NullPointerException: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        generateNullPointerException();
+        handleNullPointerException();
+    }
+}
