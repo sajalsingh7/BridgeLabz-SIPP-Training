c005d1874113b9dedecf7e857317efe6050cd4ca
Sajal <sajalsingh2704@gmail.com>
2025-06-18 11:26:11 +0530

    [Sajal] find length of string

---
+++ Strings/LengthOfString.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class LengthOfString {
+
+    public static int getLength(String str) {
+        int count = 0;
+        try {
+            while (true) {
+                str.charAt(count);
+                count++;
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String input = sc.nextLine();
+
+        int customLength = getLength(input);
+        int builtInLength = input.length();
+        System.out.println("Custom Length: " + customLength);
+        System.out.println("Built-in Length: " + builtInLength);
+    }
+}
