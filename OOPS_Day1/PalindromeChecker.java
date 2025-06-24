4753c6efb0daec3fda1655005257afa52d77c831
Sajal <sajalsingh2704@gmail.com>
2025-06-24 23:31:51 +0530

    [Sajal] Program to Check Palindrome String

---
+++ OOPS_Day1/PalindromeChecker.java
@@ -0,0 +1,25 @@
+package oops_package;
+
+public class PalindromeChecker {
+    private String text;
+
+    public PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    public boolean isPalindrome() {
+        String reversed = "";
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed += text.charAt(i);
+        }
+        return text.equalsIgnoreCase(reversed);
+    }
+
+    public void displayResult() {
+        if (isPalindrome()) {
+            System.out.println(text + " is a palindrome.");
+        } else {
+            System.out.println(text + " is not a palindrome.");
+        }
+    }
+}
