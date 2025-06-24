8e3bb3e77279c869aac7ca717b736175218686d4
Sajal <sajalsingh2704@gmail.com>
2025-06-24 22:52:22 +0530

    [Sajal] Program to Compute Area of a Circle

---
+++ OOPS_Day1/AreaOfCircle.java
@@ -0,0 +1,12 @@
+package oops_package;
+
+public class AreaOfCircle {
+	double radius;
+	AreaOfCircle(double radius){
+		this.radius=radius;
+	}
+	double Area(double radius) {
+		double area=3.14*radius*radius;
+		return area;
+	}
+}
---
+++ OOPS_Day1/MainAreaOfCircle.java
@@ -0,0 +1,12 @@
+package oops_package;
+
+import java.util.*;
+public class MainAreaOfCircle {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double radius=sc.nextDouble();
+		AreaOfCircle area=new AreaOfCircle(radius);
+		double result=area.Area(radius);
+		System.out.println(result);
+	}
+}
