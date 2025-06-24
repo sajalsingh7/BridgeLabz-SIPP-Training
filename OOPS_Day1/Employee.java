baebc9c28744accf674feede1f586df961c75e0f
Sajal <sajalsingh2704@gmail.com>
2025-06-24 21:04:26 +0530

    [Sajal] Program to Display Employee Details

---
+++ OOPS_Day1/Employee.java
@@ -0,0 +1,16 @@
+package oops_package;
+
+class Employee{
+	 String name;
+	 int id;
+	 int salary;
+	 
+	 Employee(String name, int id ,int salary){
+		 this.name=name;
+		 this.id=id;
+		 this.salary=salary;
+	 }	 
+	 void displayDetails() {
+		 System.out.println("Name: " + name + ", id: " + id + ", salary: " + salary);
+	 }	
+}
---
+++ OOPS_Day1/MainEmployee.java
@@ -0,0 +1,9 @@
+package oops_package;
+
+public class MainEmployee {
+
+	public static void main(String[] args) {
+		 Employee employee = new Employee("Sajal Singh", 9, 4000000);
+	     employee.displayDetails(); 		
+	}
+}
