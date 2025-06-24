8339693f3e1e95d03fec6039707cd314ffeeebe1
Sajal <sajalsingh2704@gmail.com>
2025-06-24 23:35:36 +0530

    [Sajal] Program to Model a Movie Ticket Booking System

---
+++ OOPS_Day1/MovieTicket.java
@@ -0,0 +1,22 @@
+package oops_package;
+
+public class MovieTicket {
+    private String movieName;
+    private String seatNumber;
+    private double price;
+
+    public MovieTicket(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public void bookTicket(String seatNumber, double price) {
+        this.seatNumber = seatNumber;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Movie Name: " + movieName);
+        System.out.println("Seat Number: " + seatNumber);
+        System.out.println("Price: ₹" + price);
+    }
+}
