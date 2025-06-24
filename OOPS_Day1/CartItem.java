0a2885b2123f7e7a320bb6fb3929fe4799d4d855
Sajal <sajalsingh2704@gmail.com>
2025-06-24 23:40:20 +0530

    [Sajal] Program to Simulate a Shopping Cart

---
+++ OOPS_Day1/CartItem.java
@@ -0,0 +1,37 @@
+package oops_package;
+
+public class CartItem {
+    private String itemName;
+    private double price;
+    private int quantity;
+
+    public CartItem(String itemName, double price, int quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public void addItem(int qty) {
+        quantity += qty;
+    }
+
+    public void removeItem(int qty) {
+        if (qty <= quantity) {
+            quantity -= qty;
+        } else {
+            quantity = 0;
+        }
+    }
+
+    public double getTotalCost() {
+        return price * quantity;
+    }
+
+    public void displayCart() {
+        System.out.println("Item Name: " + itemName);
+        System.out.println("Unit Price: ₹" + price);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Total Cost: " + getTotalCost());
+    }
+}
+
