7929e33e1be5246c3648837b20c366eb73224cd8
Sajal <sajalsingh2704@gmail.com>
2025-09-10 13:21:19 +0530

    [Sajal] finally Block Execution

---
+++ ExceptionHandling/FinallyBlock.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class FinallyBlock {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Enter first number: ");
+            int a = sc.nextInt();
+            System.out.print("Enter second number: ");
+            int b = sc.nextInt();
+            System.out.println("Result: " + (a / b));
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        } finally {
+            System.out.println("Operation completed");
+        }
+    }
+}
